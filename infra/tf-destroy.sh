72a5e7bdc7b17d6e2c10e549a3b2237c3030771a
maheshpoojaryneu <poojary.m@northeastern.edu>
2024-08-04 20:52:57 -0400

    feat: metrics server

--- infra/tf-destroy.sh
+++ infra/tf-destroy.sh
@@ -6,8 +6,6 @@ echo "####################"
 echo "Destroying Prometheus "
 echo "####################"
 
-
-terraform -chdir=./modules/addons/prometheus init
 terraform -chdir=./modules/addons/prometheus destroy --auto-approve -var-file=values.tfvars || true
 
 
@@ -15,7 +13,6 @@ echo "#################"
 echo "Destroying Kafka "
 echo "#################"
 
-terraform -chdir=./modules/kafka init
 terraform -chdir=./modules/kafka destroy --auto-approve || true
 
 
@@ -23,7 +20,6 @@ echo "#################"
 echo "Destroying ISTIO "
 echo "#################"
 
-terraform -chdir=./modules/service_mesh init
 terraform -chdir=./modules/service_mesh destroy --auto-approve || true
 
 
@@ -32,5 +28,4 @@ echo "#####################"
 echo "Destroying EksCluster "
 echo "#####################"
 
-terraform init
 terraform destroy --auto-approve -var-file=values.tfvars || true
